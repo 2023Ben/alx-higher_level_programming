ac5cfaa93a5d7b171db7bcd2dea541f15e6f097e
2023Ben <benroinvestments@gmail.com>
2024-04-23 00:38:50 +0300

    javascript learning

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const { argv } = require('process');
+
+if (argv[2] === undefined) {
+  console.log('No argument');
+} else {
+  argv.forEach((val, index) => {
+    if (index > 1) {
+      console.log(val);
+    }
+  });
+}
